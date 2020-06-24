301848c9507cad8b0e0346c39ff44c0bf3e37f19
Ara Yeressian <ayeressian2@gmail.com>
2020-06-24 23:13:47 +0400

    fix tests

--- test/uploader.test.ts
+++ test/uploader.test.ts
@@ -6,7 +6,7 @@ import uploader from '../src/uploader';
 
 describe('when uploading a directory with 3 files', () => {
   it('should call fetch api 3 times with correct arguments', async () => {
-    await uploader(resolve('test/test-upload-dir'), 'storageName');
+    await uploader(resolve('test/test-upload-dir'), 'storageName', 'key');
     expect(fetchMock).toHaveBeenCalledWith("https://storage.bunnycdn.com/storageName/test.txt", expect.anything());
     expect(fetchMock).toHaveBeenCalledWith("https://storage.bunnycdn.com/storageName/test2.txt", expect.anything());
     expect(fetchMock).toHaveBeenCalledWith("https://storage.bunnycdn.com/storageName/nestd/test3.txt", expect.anything());
