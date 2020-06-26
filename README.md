faa96661289ce61cad850ed8e8dbee38654c48c7
Ara Yeressian <ayeressian2@gmail.com>
2020-06-26 22:15:27 +0400

    added readme moved tests inside src

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # bunnycdn-storage-deploy
 
-Deploys directory to BunnyCDN storage. 
+This action deploys selected directory to BunnyCDN storage. 
+
+## Inputs
+
+### `source`
+
+**Required** The source directory folder.
+
+### `storageZoneName`
+
+**Required** The name of your storage zone where you are connecting to.
+
+### `apiKey`
+
+**Required** The storage API key.
+
+## Example usage
+````
+- name: Deploy to BunnyCDN
+  uses: ayeressian/bunnycdn-storage-deploy@latest
+  with:
+    source: "dist"
+    storageZoneName: "myzone"
+    accessKey: "${{ secrets.BUNNY_CDN_STORAGE_KEY }}"
+````
--- src/test/uploader.test.ts
+++ src/test/uploader.test.ts
@@ -1,12 +1,12 @@
 import {resolve} from 'path';
-const fetchMock = jest.fn(() => Promise.resolve());
+const fetchMock = jest.fn(() => Promise.resolve({status: 201}));
 jest.mock('node-fetch', () => fetchMock);
 
-import uploader from '../src/uploader';
+import uploader from '../uploader';
 
 describe('when uploading a directory with 3 files', () => {
   it('should call fetch api 3 times with correct arguments', async () => {
-    await uploader(resolve('test/test-upload-dir'), 'storageName', 'key');
+    await uploader(resolve('src/test/test-upload-dir'), 'storageName', 'key');
     expect(fetchMock).toHaveBeenCalledWith("https://storage.bunnycdn.com/storageName/test.txt", expect.anything());
     expect(fetchMock).toHaveBeenCalledWith("https://storage.bunnycdn.com/storageName/test2.txt", expect.anything());
     expect(fetchMock).toHaveBeenCalledWith("https://storage.bunnycdn.com/storageName/nestd/test3.txt", expect.anything());
