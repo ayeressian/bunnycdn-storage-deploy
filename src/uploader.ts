c96cc33b03cf6bc2261aec329b23f1e8b52e7663
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-10-21 18:59:46 +0500

    add "!!! RETRY ENABLED !!!" logging

--- src/uploader.ts
+++ src/uploader.ts
@@ -23,7 +23,7 @@ export default class Uploader {
       ? `${this.destination}/${entry.path}`
       : entry.path;
     info(
-      `Deploying ${entry.path} by https://${this.storageEndpoint}/${this.storageName}/${destination}`
+      `Deploying ${entry.path} by https://${this.storageEndpoint}/${this.storageName}/${destination} !!! RETRY ENABLED !!!`
     );
 
     const numRetries = 10;
